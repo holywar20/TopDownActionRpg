89f9dff70d1a1f74db8bb8d80cc80fdc5170ba94
Bryan <b.winter@mindmarker.com>
2021-01-14 00:17:18 -0500

    Lesson 10 Start

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,20 +4,39 @@ const ACCELERATION = 600
 const MAX_SPEED = 180
 const FRICTION = 600
 
+enum {
+	MOVE, ROLL, ATTACK
+}
+
+
 # Animiation Tree properties
-const IDLE = "parameters/IDLE/blend_position"
-const RUN = "parameters/RUN/blend_position"
+const BLEND_IDLE = "parameters/IDLE/blend_position"
+const BLEND_RUN = "parameters/RUN/blend_position"
+const BLEND_ATTACK = "parameters/ATTACK/blend_position"
 
 onready var animationTree : AnimationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+var state = MOVE
 var velocity : Vector2 = Vector2.ZERO
 
 func _ready():
+	animationTree.active = true
 	animationState.start("IDLE")
 
-func _physics_process(delta):
-	
+func _process( delta ):
+	match state:
+		MOVE:
+			moveState( delta )
+		ROLL:
+			pass
+		ATTACK:
+			attackState( delta )
+
+func attackState( delta ):
+	animationState.travel("ATTACK")
+
+func moveState( delta ):
 	var inputVector : Vector2 = Vector2.ZERO
 	
 	inputVector.x = Input.get_action_strength("MOVE_RIGHT") - Input.get_action_strength("MOVE_LEFT")
@@ -26,8 +45,9 @@ func _physics_process(delta):
 	inputVector = inputVector.normalized()
 
 	if( inputVector != Vector2.ZERO ):
-		animationTree.set( IDLE , inputVector )
-		animationTree.set( RUN , inputVector )
+		animationTree.set( BLEND_IDLE , inputVector )
+		animationTree.set( BLEND_RUN , inputVector )
+		animationTree.set( BLEND_ATTACK, inputVector )
 
 		animationState.travel( "RUN" )
 
@@ -38,3 +58,9 @@ func _physics_process(delta):
 		animationState.travel( "IDLE" )
 	
 	velocity = move_and_slide( velocity )
+
+	if Input.is_action_just_pressed("ATTACK"):
+		state = ATTACK
+
+func attackFinished():
+	state = MOVE
